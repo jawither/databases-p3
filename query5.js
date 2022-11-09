d8a41748514b4729c2ded78d0e4bdcefbafbd358
Jack Withers <jackwithers@Jacks-MacBook-Pro-89.local>
2022-11-09 17:30:06 -0500

    q5

--- query5.js
+++ query5.js
@@ -51,7 +51,7 @@ function oldest_friend(dbname) {
         });
 
         db.low_friends.find(
-            {friends: user_id}
+            {friends: user.user_id}
         ).forEach(function(friend) {
             if (check(friend.user_id, oldest_year, has_friend)) {
                 oldest_user = friend.user_id;
